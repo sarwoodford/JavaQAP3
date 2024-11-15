c6a7491ac9867bbacc891edc37d901e64b60143d
sara woodford <sarawoodford6@gmail.com>
2024-11-14 22:12:30 -0330

    creating student subclass (extends from person)
    assigned attributes specific to student class,
    getters, setters and toString method to display
    results

--- problem1/Student.java
+++ problem1/Student.java
@@ -1,5 +1,33 @@
 package problem1;
 
-public class Student {
-    
+public class Student extends Person{
+    protected String myIDNum;
+    protected double myGPA;
+
+    public Student(String name, int age, String gender, String idNum, double gpa){
+        super(name, age, gender);
+        myIDNum = idNum;
+        myGPA = gpa;
+    }
+
+    public String getIDNum(){
+        return myIDNum;
+    }
+
+    public void setIDNum(String idNum){
+        myIDNum = idNum;
+    }
+
+    public double getGPA(){
+        return myGPA;
+    }
+
+    public void setGPA(double gpa){
+        myGPA = gpa;
+    }
+
+    @Override
+    public String toString(){
+        return super.toString() + ", ID: " + myIDNum + ", GPA: " + myGPA;
+    }
 }
