ba8d19739728c0aa94c28e925c16f175b5016ab1
sara woodford <sarawoodford6@gmail.com>
2024-11-17 22:29:48 -0330

    Demo.java
    creating new Point and MoveablePoint objects and
    applying methods

--- problem2/Demo.java
+++ problem2/Demo.java
@@ -1,5 +1,17 @@
 package problem2;
 
 public class Demo {
-    
+    public static void main(String[] args){
+        Point point = new Point(0.5f, 2.3f);
+        System.out.println("Point 1: " + point);
+
+        point.setXY(2.1f, 2.2f);
+        System.out.println("Point Updated: " + point);
+
+        MoveablePoint moveablePoint = new MoveablePoint(1.1f, 2.2f, 3.3f, 4.4f);
+        System.out.println("Moveable Point: " + moveablePoint);
+
+        moveablePoint.move();
+        System.out.println("Point After Being Moved: " + moveablePoint);
+    }
 }
