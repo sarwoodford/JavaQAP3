1aeab5df7621c854f1e0b83b884d8dc4c1fb0db1
sara woodford <sarawoodford6@gmail.com>
2024-11-19 11:38:44 -0330

    Cicle subclass (extends from shape)
    assgined radius attribute, implemented getArea and
    getPerimeter methods using radius

--- problem3/Circle.java
+++ problem3/Circle.java
@@ -1,5 +1,21 @@
 package problem3;
 
-public class Circle {
-    
+public class Circle extends Shape{
+    private double radius;
+
+    public Circle(double radius){
+        super("Cirle");
+        this.radius = radius;
+    }
+
+    @Override
+    public double getPerimeter(){
+        return 2 * Math.PI * radius;
+    }
+
+    @Override
+    public double getArea(){
+        return Math.PI * radius * radius;
+    }
+
 }
