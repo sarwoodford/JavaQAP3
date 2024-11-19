ad76175462ee65db9fb3a6b2428f564fe80224db
sara woodford <sarawoodford6@gmail.com>
2024-11-19 11:18:39 -0330

    abstract class Shape
    assgined name attribute, getArea and getPerimeter
    abstract methods and a toString method to display
    results

--- problem3/Shape.java
+++ problem3/Shape.java
@@ -1,5 +1,17 @@
 package problem3;
 
-public class Shape {
-    
+public abstract class Shape {
+    private String name;
+
+    public Shape(String name){
+        this.name = name;
+    }
+
+    public abstract double getPerimeter();
+    public abstract double getArea();
+
+    @Override
+    public String toString(){
+        return String.format("Shape: %s, Area: %.2f, Perimeter: %.2f", name, getArea(), getPerimeter());
+    }
 }
