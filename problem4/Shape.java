9b05d2e0ac35f95ef7fd150e35750fa20ec026cb
sara woodford <sarawoodford6@gmail.com>
2024-11-19 17:15:00 -0330

    copy code from problem 3
    copying code from all Shape classes to be
    modified to implement Scalable

--- problem4/Shape.java
+++ problem4/Shape.java
@@ -1,5 +1,17 @@
 package problem4;
 
-public class Shape {
-    
+public abstract class Shape implements Scalable{
+    protected String name;
+
+    public Shape(String name){
+        this.name = name;
+    }
+
+    public abstract double getPerimeter();
+    public abstract double getArea();
+
+    @Override
+    public String toString(){
+        return String.format("Shape: %s, Area: %.2f, Perimeter: %.2f", name, getArea(), getPerimeter());
+    }
 }
