e69e5669a1b61ed0d51e97c1aa391d3990a096a1
sara woodford <sarawoodford6@gmail.com>
2024-11-19 17:59:05 -0330

    adding comments to problem 2 files

--- problem2/Demo.java
+++ problem2/Demo.java
@@ -2,15 +2,19 @@ package problem2;
 
 public class Demo {
     public static void main(String[] args){
+        // create Point (assign x and y coordinates) and print
         Point point = new Point(0.5f, 2.3f);
         System.out.println("Point 1: " + point);
 
+        // update x and y coordinates using setXY() method 
         point.setXY(2.1f, 2.2f);
         System.out.println("Point Updated: " + point);
 
+        // create a moveable point (assign x and y coordinates, as well as xSpeed and ySpeed) and print
         MoveablePoint moveablePoint = new MoveablePoint(1.1f, 2.2f, 3.3f, 4.4f);
         System.out.println("Moveable Point: " + moveablePoint);
 
+        // move the moveable point (adding the assigned speed values) and print
         moveablePoint.move();
         System.out.println("Point After Being Moved: " + moveablePoint);
     }
--- problem2/MoveablePoint.java
+++ problem2/MoveablePoint.java
@@ -1,19 +1,24 @@
 package problem2;
 
+// moveable point is a subclass of Point class
 public class MoveablePoint extends Point{
+    // assign attributes specifc to moveable point
     private float xSpeed = 0.0f;
     private float ySpeed = 0.0f;
 
+    // constructors
     public MoveablePoint(float x, float y, float xSpeed, float ySpeed){
         super(x, y);
         this.xSpeed = xSpeed;
         this.ySpeed = ySpeed;
     }
 
+    // default constructor
     public MoveablePoint(){
 
     }
 
+    // getters and setters 
     public float getXSpeed(){
         return xSpeed;
     }
@@ -30,21 +35,27 @@ public class MoveablePoint extends Point{
         this.ySpeed = ySpeed;
     }
 
+    // methods
+
+    // sets speeds for both axis'
     public void setSpeed(float xSpeed, float ySpeed){
         this.xSpeed = xSpeed;
         this.ySpeed = ySpeed;
     }
 
+    // gets speed as an array of floats
     public float[] getSpeed(){
         return new float[]{xSpeed, ySpeed};
     }
 
+    // moves the points by adding speed
     public MoveablePoint move(){
         setX(getX() + xSpeed);
         setY(getY() + ySpeed);
         return this;
     }
 
+    // tostring method to display results
     @Override 
     public String toString(){
         return super.toString() + ", speed: " + xSpeed + ", " + ySpeed;
