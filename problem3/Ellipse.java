7aad7a25bb0f719a50b3daf532b7bea2eb4a1ddf
sara woodford <sarawoodford6@gmail.com>
2024-11-19 11:53:20 -0330

    Ellipse subclass (extends from Shape)
    assgined minorAxis and majorAxis attributes which
    ensure a is always greater than or equal to b,
    and implemented getArea and getPerimeter methods
    using minorAxis and majorAxis

--- problem3/Ellipse.java
+++ problem3/Ellipse.java
@@ -1,5 +1,30 @@
 package problem3;
 
-public class Ellipse {
-    
+public class Ellipse extends Shape{
+    private double majorAxis;
+    private double minorAxis;
+
+    public Ellipse(double a, double b){
+        super("Ellipse");
+        if(a >= b){
+            this.majorAxis = a;
+            this.minorAxis = b;
+        }else{
+            this.majorAxis = b;
+            this.minorAxis = a;
+        }
+    }
+
+    @Override
+    public double getPerimeter(){
+        return Math.PI * 
+        (3 * (majorAxis + minorAxis) - 
+        Math.sqrt((3 * majorAxis + minorAxis) * 
+        (majorAxis + 3 * minorAxis)));
+    }
+
+    @Override
+    public double getArea(){
+        return Math.PI * majorAxis * minorAxis;
+    }
 }
